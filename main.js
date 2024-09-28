7d304b0698ad8747bfc3b0c8a3c9d288fa9d78cb
Theodor Kaljo <theodor.kaljo@gmail.com>
2024-09-28 10:36:01 +0300

    Adding my finished project including readme

---
+++ main.js
@@ -0,0 +1,47 @@
+
+// component 1: 
+
+const phraseOne = [
+    "Will you still love me, ", 
+    "To the left, to the left, ", 
+    "What goes around, goes around, goes around, ",
+    "I'm like a bird, I'll only fly away, ",
+    "When the sun shines, we'll shine together, "
+];
+
+// component 2:
+
+const phraseTwo = [
+    "when I'm no longer young and beautiful. \n", 
+    "everything you own in the box to the left. \n", 
+    "comes all the way back around. \n",
+    "I don't know where my soul is (soul is). \n",
+    "told you I'll be here forever. \n"
+];
+
+// component 3: 
+
+const phraseThree = [
+    "I know you will, I know you will, I know that you will, ",
+    "I don't know where my home is, ",
+    "Said I'll always be your friend, ",
+    "But since you been gone, ",
+    "Got me looking so crazy right now, "
+];
+
+// component 4:
+
+const phraseFour = [
+    "will you still love me when I got nothing but my aching soul?",
+    "and baby, all I need for you to know is I'm like a bird.",
+    "took an oath, I'ma stick it out to the end.",
+    "I can breath for the first time.",
+    "your love's got me looking so crazy right now."
+];
+
+const finalSentence = phraseOne[Math.floor(Math.random() * phraseOne.length)] 
++ phraseTwo[Math.floor(Math.random() * phraseTwo.length)]
++ phraseThree[Math.floor(Math.random() * phraseThree.length)]
++ phraseFour[Math.floor(Math.random() * phraseFour.length)];
+
+console.log(finalSentence);
---
+++ readme.md
@@ -0,0 +1,11 @@
+# Random message generator
+
+## Summary of the project
+
+This program creates a unique message based on 2000s pop hits.
+
+### Technologies used:
+> Javascript
+
+### Technologies to add:
+> HTML / CSS
